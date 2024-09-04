ec980fe30ae8350a617bf0432acab5c5622c7f52
Apostolos Karamanis <apostkaram@gmail.com>
2024-09-04 16:19:23 +0300

    Update HomeSection.js

--- src/HomeSection.js
+++ src/HomeSection.js
@@ -1,96 +1,74 @@
 import React, { useEffect, useRef, useState } from "react";
 import ContactButton from "./ContactButton";
-import useResponsiveStyles from "./useResponsiveStyles";
 
 const HomeSection = () => {
-  const [isVisible, setIsVisible] = useState(true);
+  const [isVisible, setIsVisible] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
   const sectionRef = useRef(null);
 
   useEffect(() => {
-    const currentRef = sectionRef.current;
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        setIsVisible(entry.isIntersecting);
-      });
-    });
+    const checkIfMobile = () => {
+      setIsMobile(window.innerWidth < 768); // Adjust this breakpoint as needed
+    };
 
-    if (currentRef) {
-      observer.observe(currentRef);
+    checkIfMobile();
+    window.addEventListener("resize", checkIfMobile);
+
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsVisible(entry.isIntersecting);
+      },
+      { threshold: 0.1 },
+    );
+
+    if (sectionRef.current) {
+      observer.observe(sectionRef.current);
     }
 
     return () => {
-      if (currentRef) {
-        observer.unobserve(currentRef);
+      window.removeEventListener("resize", checkIfMobile);
+      if (sectionRef.current) {
+        observer.unobserve(sectionRef.current);
       }
     };
   }, []);
 
-  const sectionStyle = useResponsiveStyles(
-    {
-      minHeight: "100vh",
-      padding: "1rem",
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      backgroundColor: "#def9c4",
-      marginTop: "-1px",
-      opacity: isVisible ? 1 : 0,
-      transition: "opacity 0.5s ease-in-out",
-    },
-    {
-      minHeight: "100vh",
-      padding: "1rem",
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      backgroundColor: "#def9c4",
-      marginTop: "-1px",
-      opacity: isVisible ? 1 : 0,
-      transition: "opacity 0.5s ease-in-out",
-    },
-  );
+  const sectionStyle = {
+    minHeight: "100vh",
+    padding: "1rem",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "flex-start",
+    opacity: isVisible ? 1 : 0,
+    transition: "opacity 1s ease-in-out",
+    marginRight: isMobile ? "0" : "50%",
+  };
 
-  const contentStyle = useResponsiveStyles(
-    {
-      maxWidth: "100%",
-      marginLeft: "5%",
-      marginRight: "5%",
-      textAlign: "left",
-    },
-    {
-      maxWidth: "32rem",
-      marginLeft: "10%",
-      textAlign: "left",
-    },
-  );
+  const contentStyle = {
+    maxWidth: isMobile ? "100%" : "32rem",
+    display: "flex",
+    flexDirection: "column",
+    textAlign: "left",
+    padding: isMobile ? "5%" : "10%",
+    marginTop: isMobile ? "10%" : "15%",
+  };
 
-  const h1Style = useResponsiveStyles(
-    {
-      fontSize: "2rem",
-      fontWeight: "bold",
-      marginBottom: "1rem",
-      color: "#303642",
-    },
-    {
-      fontSize: "3rem",
-      fontWeight: "bold",
-      marginBottom: "1rem",
-      color: "#303642",
-    },
-  );
+  const h1Style = {
+    fontSize: isMobile ? "40px" : "65px",
+    color: "#303642",
+    marginBottom: "1rem",
+  };
 
-  const h3Style = useResponsiveStyles(
-    {
-      fontSize: "1rem",
-      marginBottom: "1rem",
-      color: "#303642",
-    },
-    {
-      fontSize: "1.5rem",
-      marginBottom: "1rem",
-      color: "#303642",
-    },
-  );
+  const h3Style = {
+    fontSize: isMobile ? "20px" : "30px",
+    color: "#303642",
+    marginBottom: "1rem",
+  };
+
+  const buttonContainerStyle = {
+    marginTop: "2rem",
+  };
 
   return (
     <section id="home" ref={sectionRef} style={sectionStyle}>
@@ -103,7 +81,7 @@ const HomeSection = () => {
           Your fleet, our technology - together, we sail towards a smarter,
           safer and more sustainable future.
         </h3>
-        <div style={{ marginTop: "2rem" }}>
+        <div style={buttonContainerStyle}>
           <ContactButton />
         </div>
       </div>
